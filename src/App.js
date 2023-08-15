2b6d34b0351053887532d602d02033f4430fd149
Asnoyon <blackpain1100@gmail.com>
2023-08-15 11:45:58 +0600

    app and form updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Form from "./components/Form";
+import FormList from "./components/FormList";
 function App() {
   return (
     <div>
       <Form />
+      <FormList />
     </div>
   );
 }
--- src/components/FormList.js
+++ src/components/FormList.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function FormList() {
-  return <div>Hello</div>;
+  let curDate = new Date().toLocaleDateString();
+  let curTime = new Date().toLocaleTimeString();
+  return (
+    <div>
+      <h1>Today time and Date Showing</h1>
+      <p>Today date is {curDate}</p>
+      <p>Today time is {curTime}</p>
+    </div>
+  );
 }
 
 export default FormList;
